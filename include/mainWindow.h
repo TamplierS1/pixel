6396ef676a3438e1f73e4feea431fb7c8c0ece46
TamplierS1 <azamat.nabiullin.97@mail.ru>
2022-02-03 11:22:02 +0000

    Open files.
    - Open files using `Open` button in `File` menu item.
    - Close the app using `Quit` button in `File` menu item.
    - Updated `README.md`.

--- include/mainWindow.h
+++ include/mainWindow.h
@@ -1,10 +1,23 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QTextEdit>
 
 class MainWindow : public QMainWindow
 {
 public:
     MainWindow(int width = 800, int height = 600, std::string_view title = "Main Window",
                QWidget* parent = nullptr);
+
+private:
+    void createMenus();
+    void createEditor();
+
+    void openFileDialog();
+    void openFileForEditing(const QString& name);
+
+    QString mDefaultStartDir = ".";
+    QString mPathToCurrentFile;
+
+    QTextEdit* mEditor = nullptr;
 };
--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -1,8 +1,12 @@
 #include <string_view>
+#include <fstream>
+#include <sstream>
 
 #include <QMenu>
 #include <QMenuBar>
 #include <QApplication>
+#include <QTextEdit>
+#include <QFileDialog>
 
 #include "mainWindow.h"
 
@@ -12,10 +16,43 @@ MainWindow::MainWindow(int width, int height, std::string_view title, QWidget* p
     this->resize(width, height);
     this->setWindowTitle(QString{title.data()});
 
-    QAction* quit = new QAction{"&Quit", this};
+    createMenus();
+    createEditor();
+}
 
+void MainWindow::createMenus()
+{
     QMenu* fileMenu = menuBar()->addMenu("&File");
-    fileMenu->addAction(quit);
 
+    QAction* quit = new QAction{"&Quit", this};
+    fileMenu->addAction(quit);
     connect(quit, &QAction::triggered, qApp, QApplication::quit);
+
+    QAction* open = new QAction{"&Open", this};
+    fileMenu->addAction(open);
+    connect(open, &QAction::triggered, this, &MainWindow::openFileDialog);
+}
+
+void MainWindow::createEditor()
+{
+    mEditor = new QTextEdit{this};
+    setCentralWidget(mEditor);
+}
+
+void MainWindow::openFileDialog()
+{
+    mPathToCurrentFile =
+        QFileDialog::getOpenFileName(this, "Open File to Edit", mDefaultStartDir);
+
+    openFileForEditing(mPathToCurrentFile);
+}
+
+void MainWindow::openFileForEditing(const QString& name)
+{
+    std::fstream file{name.toStdString()};
+
+    std::stringstream contents;
+    contents << file.rdbuf();
+
+    mEditor->setText(contents.str().c_str());
 }
